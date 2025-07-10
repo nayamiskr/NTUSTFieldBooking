c6700a47410899ceaf9fb192d834cae7bc2259a7
nayamiskr <bigbird0887@gmail.com>
2025-07-10 16:31:25 +0800

    add nav

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h1>帳號列表</h1>
       {accounts.map((item) => (
         <div key={item.UserId}>
@@ -29,6 +30,26 @@ function App() {
         </div>
       ))}
     </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <h2>NTUST 場地預約系統</h2>
+      </div>
+      <ul className="navbar-center">
+        <li><a href="#">場地</a></li>
+        <li><a href="#">公告</a></li>
+        <li><a href="#">我的預約</a></li>
+      </ul>
+      <div className="navbar-right">
+        <div className="toggle-container">
+          <label htmlFor="toggle" className="toggle-label" />
+        </div>
+      </div>
+    </nav>
   );
 }
 
