15086543b7708120630d08242b9a4e85c830be3d
nayamiskr <bigbird0887@gmail.com>
2026-01-08 21:49:52 +0800

    更新路由

--- src/page/homepage.jsx
+++ src/page/homepage.jsx
@@ -1,11 +1,9 @@
-import { use, useRef } from "react";
 import FieldFilterButton from "../components/fieldFilterButton";
 import Navbar from "../components/navbar";
-import { useLocation, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 
 function Homepage() {
   const { fieldType } = useParams();
-  const refScroll = useRef(null);
   return (
     <div className="homepage">
         <Navbar />
